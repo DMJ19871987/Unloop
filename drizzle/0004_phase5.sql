abcccdf07e891627dce6a5dd7db6c761fe2d20a1
DMJ19871987 <davidjohnson1987@googlemail.com>
2026-07-10 22:46:02 +0100

    Add Drizzle migration metadata

--- drizzle/0004_phase5.sql
+++ drizzle/0004_phase5.sql
@@ -1,3 +1,3 @@
-ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "past_due_since" timestamp;
-ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "last_checkin_sent_at" timestamp;
+ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "past_due_since" timestamp;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "last_checkin_sent_at" timestamp;--> statement-breakpoint
 ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "trial_reminder_sent_at" timestamp;
